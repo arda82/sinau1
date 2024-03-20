7e90da3e2e69a0278256ca0b2403a7d0ba0260fb
arda82 <arda.alimana@gmail.com>
2024-03-20 20:03:02 +0700

    Add files via upload

---
+++ hitung.py
@@ -0,0 +1,40 @@
+from datetime import date
+
+def hitung(mulai):
+  """
+  Program untuk menghitung umur dalam hari.
+  Args:
+    mulai: Tanggal mulai hitung dengan format tgl,bln,thn
+  Returns:
+    Jumlah hari dari mulai hitungan sampai hari ini.
+  """
+
+  # Cek format tanggal
+  try:
+    tanggal, bulan, tahun = mulai.split(",")
+    date(int(tahun), int(bulan), int(tanggal))
+  except ValueError:
+    raise ValueError("Format tanggal tidak valid (harus tgl,bln,thn)")
+
+  # Nilai awal hitungan
+  nilai_awal = date(int(tahun), int(bulan), int(tanggal))
+
+  # Tanggal hari ini
+  hari_ini = date.today()
+
+  # Menghitung hari
+  umur_hari = hari_ini - nilai_awal
+
+  return umur_hari.days
+
+
+# Input tanggal mulai
+#mulai = input("Masukkan tanggal mulai (format tgl,bln,thn): ")
+
+# Hitung umur
+#try:
+#  umur = hitung(mulai)
+#except ValueError as e:
+#  print(e)
+#else:
+#  print(f"Umur tanaman adalah {umur} hari")
