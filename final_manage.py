e1e2bdd1fd433a251762630b3f280824b237bb4f
arda82 <arda.alimana@gmail.com>
2024-08-03 15:19:53 +0700

    Update final_manage.py

--- final_manage.py
+++ final_manage.py
@@ -3,7 +3,7 @@ import os
 from utils import *
 
 def main():
-    db_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'sawah.dat')
+    db_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'my_data.dat')
     
     selected_table = None
 
